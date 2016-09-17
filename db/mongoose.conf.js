7ab968befcd11051fe14e6ce60e237a9bfd11f90
marcuscaum <marcuslucas.jb@gmail.com>
2016-09-17 19:44:30 -0300

    RETURN OF J. JONAH JAMESON

--- db/mongoose.conf.js
+++ db/mongoose.conf.js
@@ -2,6 +2,6 @@
 
 const Mongoose = require('mongoose');
 
-const MONGODB_URI = process.env.MONGOLAB_URI || 'mongodb://127.0.0.1/reactstore';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1/reactstore';
 
 Mongoose.connect(MONGODB_URI);
